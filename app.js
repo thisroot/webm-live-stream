1a1008607e0aeb3afc29f2e9b758f5db4fea1186
Egor <mail@nebesa.me>
2017-07-20 09:40:28 +0300

    first

---
+++ app.js
@@ -0,0 +1,71 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import views from 'koa-views'
+import serve from 'koa-static'
+import range from 'koa-range';
+
+const app = new Koa();
+//const io = new IO();
+const router = new Router();
+
+//io.attach(app);
+
+
+require('./utils/signaling-server.js')(app);
+
+app
+    .use(serve('./public'))
+    .use(range)
+    .use(logger())
+    .use(views(__dirname + '/views', {
+        map: {
+            html: 'handlebars'
+        }
+    }));
+
+router.get('/streamrec', (ctx) => {
+    return ctx.render('streamrec.html', {title: "Streamrec"});
+});
+
+router.get('/broadcast', (ctx) => {
+    return ctx.render('broadcast.html', {title: "Broadcasting"});
+});
+
+
+router.get('/record', (ctx) => {
+    return ctx.render('index.html', {title: "Recorder"});
+});
+
+
+router.post('/publish', (ctx, next) => {
+
+    console.log(ctx.request.query, ctx.request.body);
+
+    let body = ctx.request.body;
+    if (!body.name) {
+        ctx.status = 401
+    }
+
+    //ctx.status = 201
+    ctx.redirect("rtmp://127.0.0.1/publish/" + body.name,302)
+
+});
+
+router.post('/publish_done', (ctx, next) => {
+    console.log(ctx.request.query, ctx.request.body);
+    ctx.status = 200
+});
+
+app
+    .use(bodyParser())
+    .use(router.routes())
+    .use(router.allowedMethods("GET, POST, PUT, DELETE"));
+
+
+app.listen(8986, function () {
+    console.log("server started")
+});
+
+
---
+++ utils/record/index.js
@@ -0,0 +1,53 @@
+/**
+ * Created by User on 11.07.2017.
+ */
+import path from 'path'
+import FFmpeg from 'fluent-ffmpeg';
+import fs from 'fs'
+
+export function writeToDisk(dataURL, fileName) {
+    let fileExtension = fileName.split('.').pop(),
+        fileRootNameWithBase = './uploads/' + fileName,
+        filePath = fileRootNameWithBase,
+        fileID = 2,
+        fileBuffer;
+
+    // @todo return the new filename to client
+    while (fs.existsSync(filePath)) {
+        filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
+        fileID += 1;
+    }
+
+    dataURL = dataURL.split(',').pop();
+    fileBuffer = new Buffer(dataURL, 'base64');
+    fs.writeFileSync(filePath, fileBuffer);
+
+    console.log('filePath', filePath);
+}
+
+export function merge(socket, fileName) {
+
+    var audioFile = path.join('./uploads', fileName + '.wav'),
+        videoFile = path.join('./uploads', fileName + '.webm'),
+        mergedFile = path.join('./uploads', fileName + '-merged.webm');
+
+    new FFmpeg({
+        source: videoFile
+    })
+        .addInput(audioFile)
+        .on('error', function (err) {
+            socket.emit('ffmpeg-error', 'ffmpeg : An error occurred: ' + err.message);
+        })
+        .on('progress', function (progress) {
+            socket.emit('ffmpeg-output', Math.round(progress.percent));
+        })
+        .on('end', function () {
+            socket.emit('merged', fileName + '-merged.webm');
+            console.log('Merging finished !');
+
+            // removing audio/video files
+            fs.unlink(audioFile);
+            fs.unlink(videoFile);
+        })
+        .saveToFile(mergedFile);
+}
